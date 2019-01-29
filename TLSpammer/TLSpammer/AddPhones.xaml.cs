4699e2fc13042bf6502b25cce47974f0fb510c98
Vladislav <38260058+ACrYoZ@users.noreply.github.com>
2019-01-29 13:22:26 +0300

    Add files via upload

---
+++ TLSpammer/TLSpammer/AddPhones.xaml.cs
@@ -0,0 +1,155 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using TeleSharp.TL;
+using TLSharp.Core;
+
+namespace TLSpammer
+{
+    /// <summary>
+    /// Логика взаимодействия для AddPhones.xaml
+    /// </summary>
+    
+    public partial class AddPhones : Window
+    {
+        TelegramClient client;
+        List<String> users = new List<string>();
+
+        public AddPhones()
+        {
+            InitializeComponent();
+
+            //Отключаем кнопку чтобы у пользователя не было возможности запустить цикл спама до того момента, пока
+            //он не выберет целевую аудиторию (файл)
+            Btn_BeginSpam.IsEnabled = false;
+            
+        }
+
+        //Логика завершения приложения
+        private void Mni_Exit_Click(object sender, RoutedEventArgs e)
+        {
+            App.Current.Shutdown();
+        }
+
+        //Логика открытия файла и добавления пользователей
+        private void Btn_OpenFile_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+
+            //Разрешаем выбор только текстовых документов
+            ofd.Filter = "Text File (*txt)|*.txt";
+
+            if (ofd.ShowDialog() == true)
+            {
+                //Отображаем путь к выбранному файлу
+                Txb_FPath.Text = ofd.FileName;
+                
+                using(StreamReader sr = new StreamReader(ofd.FileName))
+                {
+                    //Временная строка, для чтения в неё
+                    String line;
+
+                    while((line = sr.ReadLine()) != null)
+                    {
+                        //Убираем лишние пробелы во избежание проблем с отправкой
+                        //т.е. должно быть так: @nickname
+                        //            а не так:      @nickname
+                        line = line.Replace(" ", string.Empty);
+
+                        //Если строка не пустая - добавляем в список пользователей и увеличиваем прогресс бар
+                        if (line != "" && line[0].Equals('@'))
+                        {
+                            users.Add(line);
+
+                            Progr_Users.Value = users.Count();
+                        }
+
+                    }
+                }
+
+                //Если всё прошло хорошо - активируем кнопку рассылки спама
+                Progr_Users.Value = 100;
+                Btn_BeginSpam.IsEnabled = true;
+            }//if
+        }
+
+        //Логика рассылки сообщений
+        private async void Btn_BeginSpam_Click(object sender, RoutedEventArgs e)
+        {
+           try
+           {
+                Txb_Waiting.IsEnabled = false;
+
+               // получаем текущую сессию
+               var session = new FileSessionStore();
+               //Создаем клиента
+               var cl = new TelegramClient(MainWindow.API_ID, MainWindow.API_HASH, session, "session");
+
+               //переопределяем клиента
+               client = cl;
+
+                int time_to_sleep;
+
+                int.TryParse(Txb_Waiting.Text, out time_to_sleep);
+
+               //подключаем клиента
+               await client.ConnectAsync();
+
+                int imax = users.Count;
+
+               foreach (string username in users)
+               {
+                    //ищем пользователя по его Никнейму
+                    var found = await client.SearchUserAsync(username, 1);
+
+                    if (found.Users.Count > 0)
+                    {
+                        //получаем пользователя
+                        var u = found.Users.lists.OfType<TLUser>().First();
+
+                        if (!u.Bot)
+                        {
+                            //отправляем ему сообщение
+                            await client.SendMessageAsync(new TLInputPeerUser() { UserId = u.Id, AccessHash = (long)u.AccessHash }, Txb_Message.Text);
+                            Thread.Sleep(time_to_sleep);
+                        }
+                    }
+
+                }
+
+                Progr_Spam.Value = 100;
+
+                Txb_Waiting.IsEnabled = true;
+           }
+           catch (Exception ex)
+           {
+               MessageBox.Show(ex.Message, "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+           }
+        }
+
+        private void Mni_About_Click(object sender, RoutedEventArgs e)
+        {
+            AboutWindow aw = new AboutWindow();
+            aw.ShowDialog();
+        }
+
+        private void Mni_Guide_Click(object sender, RoutedEventArgs e)
+        {
+            GuideWindow gw = new GuideWindow();
+            gw.ShowDialog();
+        }
+    }
+}
---
+++ TLSpammer/TLSpammer/MainWindow.xaml.cs
@@ -0,0 +1,124 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using TeleSharp.TL;
+using TeleSharp.TL.Contacts;
+using TeleSharp.TL.Messages;
+using TLSharp.Core;
+using TLSharp.Core.Network;
+
+namespace TLSpammer
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        //Данные для работы с телеграмм клиентом
+        public const int API_ID = 571639;
+        public const string API_HASH = "7674768b0ead3a637c270a5f41783083";
+
+        //Сам клиент. Нужен для подключения к сервису
+        TelegramClient client;
+
+        //Сами пользователи
+        public TLUser user;
+
+        //Их хэш для создания пользователя
+        string hash_1;
+
+        public  MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Btn_Exit_Click(object sender, RoutedEventArgs e)
+        {
+            App.Current.Shutdown();
+        }
+
+        //Обработчик логики авторизации
+        private async void Btn_Auth_Click(object sender, RoutedEventArgs e)
+        {
+            if (Txb_Code1.Text.Length > 0)
+            {
+
+                if (hash_1 != null)
+                {
+                    //Подключаемся к существующему файлу сессии если она есть и данные совпадают. Если же файла нет - 
+                    //он создается, если файл есть но данные не идентичны - перезаписываем сессию.
+                    var session = new FileSessionStore();
+                    //Подключаем клиент с указанием файла сессии.
+                    var cl = new TelegramClient(API_ID, API_HASH, session, "session");
+
+                    //Отправляем запрос на подключение клиента
+                    await client.ConnectAsync();
+
+                    //Авторизовываем пользователя
+                    var user1 = await client.MakeAuthAsync(Txb_Phone1.Text, hash_1, Txb_Code1.Text);
+                    
+                    //Запоминаем пользователя и клиент в соответствующие переменные
+                    user = user1;
+
+                    client = cl;
+
+                    //Запускаем следующее окно
+                    AddPhones nextWin = new AddPhones();
+                    nextWin.Show();
+                }
+                else
+                {
+                    MessageBox.Show("Что-то не так. Попробуйте снова", 
+                                    "Ошибка", 
+                                    MessageBoxButton.OK, 
+                                    MessageBoxImage.Error, 
+                                    MessageBoxResult.OK);
+                }
+            }
+        }
+
+        private async void Btn_SendCodes_Click(object sender, RoutedEventArgs e)
+        { 
+
+            if (Txb_Phone1.Text.Length > 0)
+            {
+                try
+                {
+                    //Подключаем клиент.
+                    client = new TelegramClient(API_ID, API_HASH);
+                    await client.ConnectAsync();
+
+                    //Высылаем код подтверждения
+                    var hash1 = await client.SendCodeRequestAsync(Txb_Phone1.Text);
+                    //Запоминаем хеш авторизуемового пользователя. Понадобится для его подключения
+                    hash_1 = hash1;
+
+                } catch (Exception ex)
+                {
+                    if(ex is FloodException)
+                    {
+                        MessageBox.Show("Слишком частое подключение к сессии. Придется подождать." + ex.Message, "FloodException", MessageBoxButton.OK, MessageBoxImage.Error);
+                    } else if (ex is InvalidOperationException)
+                    {
+                        MessageBox.Show(ex.Message, "InvalidOperationException", MessageBoxButton.OK, MessageBoxImage.Error);
+                    }
+
+                    MessageBox.Show(ex.ToString());
+                } 
+            }
+
+        }
+    }
+}
